4c3ed1d4e40ebbaa9b49edbc7f8c1c816e4cdb9a
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-11-18 11:18:56 +0100

    Clean up in BeersPresenter

--- lib/src/main/java/net/chmielowski/beer/ui/beers/BeersPresenter.java
+++ lib/src/main/java/net/chmielowski/beer/ui/beers/BeersPresenter.java
@@ -2,20 +2,12 @@ package net.chmielowski.beer.ui.beers;
 
 final class BeersPresenter {
 
-    private final ChangeDataObserverAction mAction;
-
     BeersPresenter(final BeersView view,
             final ChangeDataObserverAction action) {
-        mAction = action;
-        configureShowingBeers(view);
-    }
-
-    private void configureShowingBeers(final BeersView view) {
         view.showLoading(true);
         view.sortingMethodNumber()
             .map(new MethodNumberToSortingFunction())
-            .subscribe(mAction);
-
+            .subscribe(action);
     }
 
 }
