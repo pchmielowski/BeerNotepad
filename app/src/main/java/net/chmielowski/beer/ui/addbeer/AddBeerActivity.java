072b22422a7e67473fb4484487227c2e792e8058
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-11-19 19:21:53 +0100

    Image thumbnail is displayed on AddBeerActivity

--- app/src/main/java/net/chmielowski/beer/ui/addbeer/AddBeerActivity.java
+++ app/src/main/java/net/chmielowski/beer/ui/addbeer/AddBeerActivity.java
@@ -1,9 +1,11 @@
 package net.chmielowski.beer.ui.addbeer;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import net.chmielowski.beer.BeerApplication;
 import net.chmielowski.beer.R;
@@ -14,6 +16,7 @@ import javax.inject.Inject;
 import rx.functions.Action1;
 
 public final class AddBeerActivity extends AppCompatActivity {
+    final int requestImageCapture = 1;
     @Inject
     Beers mBeers;
 
@@ -35,10 +38,20 @@ public final class AddBeerActivity extends AppCompatActivity {
         );
     }
 
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode,
+            final Intent data) {
+        if (requestCode == requestImageCapture && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ((ImageView) findViewById(R.id.add_iv_picture)).setImageBitmap(
+                    imageBitmap);
+        }
+    }
+
     class TakePictureAction implements Action1<Void> {
         @Override
         public void call(final Void aVoid) {
-            final int requestImageCapture = 1;
             Intent takePictureIntent = new Intent(
                     MediaStore.ACTION_IMAGE_CAPTURE);
             if (takePictureIntent.resolveActivity(getPackageManager())
